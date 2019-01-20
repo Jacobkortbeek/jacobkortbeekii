1f688f48af00dfd99310c2f5e0a234c75205c650
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-01-20 20:28:00 +0000

    parallax hero

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -1,16 +1,16 @@
-<div class="jumbotron jumbotron-fluid" style="background-image: url('./assets/img/parralax-header/monunt.jpg');">
+<div class="jumbotron jumbotron-fluid" style="background-image: url('<?php the_sub_field('main_background_image'); ?>');">
   <div class="parra-cont">
     <div class="heroTextCont">
       <h1 class="col-12"><span class="align-middle"><?php the_sub_field('hero_text'); ?></span></h1>
     </div>
-    <div class="foreGround-mountain" style="background-image: url('./assets/img/parralax-header/mountiain.png');">
+    <div class="foreGround-mountain" style="background-image: url('<?php the_sub_field('parallax_image_one'); ?>');">
 
     </div>
 
-    <div class="main-mountain" style="background-image: url('./assets/img/parralax-header/mountina.png');">
+    <div class="main-mountain" style="background-image: url('<?php the_sub_field('parallax_image_two'); ?>');">
 
     </div>
-    <div class="top-moutain" style="background-image: url('./assets/img/parralax-header/imgmousn.png');">
+    <div class="top-moutain" style="background-image: url('<?php the_sub_field('parallax_image_three'); ?>');">
 
     </div>
   </div>
