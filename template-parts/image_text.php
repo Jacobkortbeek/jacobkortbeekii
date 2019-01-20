9bb4a3140ec5ca1f88e3ef1cdfafb178557096a6
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-01-20 20:49:57 +0000

    img text constructor

---
+++ template-parts/image_text.php
@@ -0,0 +1,26 @@
+<div class="container pad-top-cont">
+  <div class="row img-text">
+    <div class="col-lg-6 margin-bot">
+      <div class="hvr-img fullCenter">
+        <img class="round shadow" src="./assets/img/lukasz-szmigiel-33891-unsplash.jpg" alt="">
+        <i class="far fa-hand-pointer tapBlog dNonePc"></i>
+        <ul class="circle-container">
+          <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+          <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+          <li><a href="#"><i class="fab fa-github"></i></a></li>
+          <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+          <li><a href="#"><i class="fab fa-codepen"></i></a></li>
+          <li><a href="#"><i class="fab fa-whatsapp"></i></a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="col-lg-6">
+      <h1>Header</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <br />
+      <button type="button" class="btn btn-primary float-right">Read More About Me</button>
+    </div>
+  </div>
+</div>
