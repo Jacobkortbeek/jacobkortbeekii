b85c2a8e024bbf322d5f0aa15ed7c8032e959d77
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-01-20 20:20:20 +0000

    homepage and constructor set up

---
+++ template-parts/constructor.php
@@ -0,0 +1,23 @@
+<?php
+  if( have_rows('page_builder') ):
+
+    while( hvae_rows('page_builder') ) : the_row();
+
+      if( get_row_layout() == 'hero' ):
+
+         get_template_part( 'template-parts/hero', 'none' );
+
+      elseif( get_row_layout() == 'nextpart' ):
+        this is the next part
+
+      endif;
+
+    endwhile;
+
+    else : ?>
+
+    <p>there are no parts</p>
+    
+  <?php
+  endif;
+?>
---
+++ template-parts/hero.php
@@ -0,0 +1,17 @@
+<div class="jumbotron jumbotron-fluid" style="background-image: url('./assets/img/parralax-header/monunt.jpg');">
+  <div class="parra-cont">
+    <div class="heroTextCont">
+      <h1 class="col-12"><span class="align-middle">Jacob Kortbeek static</span></h1>
+    </div>
+    <div class="foreGround-mountain" style="background-image: url('./assets/img/parralax-header/mountiain.png');">
+
+    </div>
+
+    <div class="main-mountain" style="background-image: url('./assets/img/parralax-header/mountina.png');">
+
+    </div>
+    <div class="top-moutain" style="background-image: url('./assets/img/parralax-header/imgmousn.png');">
+
+    </div>
+  </div>
+</div>
