22f33c6615bcabb2140978c4bb5a7d8ef768381c
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-01-20 20:29:55 +0000

    parallax hero

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -3,14 +3,14 @@
     <div class="heroTextCont">
       <h1 class="col-12"><span class="align-middle"><?php the_sub_field('hero_text'); ?></span></h1>
     </div>
-    <div class="foreGround-mountain" style="background-image: url('<?php the_sub_field('parallax_image_one'); ?>');">
+    <div class="foreGround-mountain" style="background-image: url('<?php the_sub_field('parallax_image_three'); ?>');">
 
     </div>
 
     <div class="main-mountain" style="background-image: url('<?php the_sub_field('parallax_image_two'); ?>');">
 
     </div>
-    <div class="top-moutain" style="background-image: url('<?php the_sub_field('parallax_image_three'); ?>');">
+    <div class="top-moutain" style="background-image: url('<?php the_sub_field('parallax_image_one'); ?>');">
 
     </div>
   </div>
