57094eaa55ed6b8ea2ff6fe53545a0bbdf70c78e
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-01-20 20:25:21 +0000

    homepage and constructor set up

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -1,7 +1,7 @@
 <div class="jumbotron jumbotron-fluid" style="background-image: url('./assets/img/parralax-header/monunt.jpg');">
   <div class="parra-cont">
     <div class="heroTextCont">
-      <h1 class="col-12"><span class="align-middle">Jacob Kortbeek static</span></h1>
+      <h1 class="col-12"><span class="align-middle"><?php the_sub_field('hero_text'); ?></span></h1>
     </div>
     <div class="foreGround-mountain" style="background-image: url('./assets/img/parralax-header/mountiain.png');">
 
