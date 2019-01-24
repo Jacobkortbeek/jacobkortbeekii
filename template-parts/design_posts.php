c216f1b6ab5a9ec71b929a2a0209407585e2ae96
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-01-24 13:46:10 +0000

    design posts constructor

--- template-parts/design_posts.php
+++ template-parts/design_posts.php
@@ -22,27 +22,43 @@
       ?>
       <?php $i=0; if( $query->have_posts() ) : while( $query->have_posts() ) : $query->the_post(); ?>
         <?php if( $i < 4 ) : ?>
-      <div class="col-lg-6">
-        <div class="portCont">
-          <h4 class="text-center"><?php the_title(); ?></h4>
-          <div class="fullCenter imgCont shadow">
-            <?php the_post_thumbnail(); ?>
-            <i class="far fa-hand-pointer tap dNonePc"></i>
-            <div class="overlay">
-              <h4 class="centered"><?php the_field( 'hover_text' ); ?></h4>
-              <a href="<?php the_permalink(); ?>" class="read"><i class="fab fa-readme"></i></a>
+          <div class="col-md-6">
+            <div class="portCont">
+              <h4 class="text-center"><?php the_title(); ?></h4>
+              <div class="fullCenter imgCont shadow">
+                <?php the_post_thumbnail(); ?>
+                <i class="far fa-hand-pointer tap dNonePc"></i>
+                <div class="overlay">
+                  <h4 class="centered"><?php the_field( 'hover_text' ); ?></h4>
+                  <a href="<?php the_permalink(); ?>"><i class="fab fa-readme"></i></a>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
+
     <?php endif; ?>
       <?php $i++;?>
       <?php endwhile; endif; wp_reset_postdata(); ?>
 
     </div>
+    <!-- <div class="row">
+
+      <div class="col-md-6">
+        <div class="portCont">
+          <h4 class="text-center">Title of Design</h4>
+          <div class="fullCenter imgCont shadow">
+            <img src="./assets/img/lukasz-szmigiel-33891-unsplash.jpg" alt="">
+            <div class="overlay">
+              <h4 class="centered">This is the test text</h4>
+              <a href="#"><i class="fab fa-readme"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div> -->
     <div class="row pad-top-cont">
       <div class="col-md-12">
-        <a href="<?php the_sub_field('design_button_link'); ?> "><button type="button" class="btn btn-primary float-right"><?php the_sub_field('design_button_text'); ?></button></a>
+        <button type="button" class="btn btn-primary float-right">See My Desgin Library</button>
       </div>
     </div>
   </div>
