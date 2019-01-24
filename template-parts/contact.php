68308613585c0b5762b15b1e6e4eb458c826f220
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-01-24 15:15:18 +0000

    contact constructor

--- template-parts/contact.php
+++ template-parts/contact.php
@@ -3,19 +3,18 @@
     <div class="col-md-6 contactItemCont">
       <?php if( have_rows('contact_information') ):
               while( have_rows('contact_information') ) : the_row();
-         ?>
-         <div class="contactItem">
-           <i class="<?php the_sub_field('contact_fa_icon'); ?>"></i>
-           
+      ?>
+        <div class="contactItem">
+          <i class="<?php the_sub_field('contact_fa_icon'); ?>"></i>
+        </div>
        <?php endwhile; endif;?>
-
     </div>
-  </div>
-  <div class="row">
-    <div class="col-md-12">
-      <form class="" action="index.html" method="post">
-        This is where the contact form7 goes
-      </form>
+    <div class="row">
+      <div class="col-md-12">
+        <form class="" action="index.html" method="post">
+          This is where the contact form7 goes
+        </form>
+      </div>
     </div>
   </div>
 </div>
