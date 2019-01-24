e6c5adfdc13a0b328a619f691245a0ece5ad6a4f
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-01-24 11:07:54 +0000

    remove empty p tags

--- template-parts/blog_posts.php
+++ template-parts/blog_posts.php
@@ -35,7 +35,7 @@
           <div class="jk-card-reveal">
             <h2><?php the_title(); ?></h2>
             <?php the_content(); ?>
-            <a href="<?php the_sub_field('blog_button_link'); ?>">Read More</a>
+            <a href="<?php the_permalink(); ?>">Read More</a>
           </div>
         </div>
 
@@ -44,57 +44,10 @@
       <?php $i++;?>
       <?php endwhile; endif; wp_reset_postdata(); ?>
 
-
-
-      <!-- <div class="col-lg-4">
-
-        <div class="jk-card bottomBorderRandom shadow margin-bot">
-          <div class="jk-card-image">
-            <img src="./assets/img/lukasz-szmigiel-33891-unsplash.jpg" alt="">
-            <i class="far fa-hand-pointer tapBlog dNonePc"></i>
-          </div>
-          <div class="jk-card-content">
-            <h2>Title</h2>
-            <p>
-              exerpt
-            </p>
-          </div>
-          <div class="jk-card-reveal">
-            <h2>title</h2>
-            <p>more experpt</p>
-            <a href="#">Read More</a>
-          </div>
-        </div>
-
-      </div>
-      <div class="col-lg-4">
-
-        <div class="jk-card bottomBorderRandom shadow-lg margin-bot">
-          <div class="jk-card-image">
-            <img src="./assets/img/lukasz-szmigiel-33891-unsplash.jpg" alt="">
-            <i class="far fa-hand-pointer tapBlog dNonePc"></i>
-          </div>
-          <div class="jk-card-content">
-            <h2>Title</h2>
-            <p>
-              exerpt
-            </p>
-          </div>
-          <div class="jk-card-reveal">
-            <h2>title</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-            <a href="#">Read More</a>
-          </div>
-        </div>
-
-      </div> -->
     </div>
     <div class="row pad-top-cont">
       <div class="col-lg-12">
-        <button type="button" class="btn btn-primary float-right">See All My Blog Posts</button>
+        <a href="<?php the_sub_field('blog_button_link'); ?>"><button type="button" class="btn btn-primary float-right">See All My Blog Posts</button></a>
       </div>
     </div>
   </div>
