0874f7e200eea6e2ed3e15cd24375e9419a7d3bf
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-01-24 10:57:19 +0000

    remove empty p tags

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
   add_theme_support( 'post-thumbnails' );
 
   function custom_excerpt_length( $length ) {
-        return 5;
+        return $length;
     }
     add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
 
