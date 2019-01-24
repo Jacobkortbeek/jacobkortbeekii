caa3be73ac2315e594136170f4d1b0b1acf4ab07
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-01-24 12:59:16 +0000

    portfolio posts constructor

--- template-parts/portfolio_posts.php
+++ template-parts/portfolio_posts.php
@@ -6,21 +6,41 @@
       </div>
     </div>
     <div class="row">
+      <?php
+
+        $num_posts = get_option( 'posts_per_page' );
+
+        $args = array(
+          'post_type' => 'portfolio',
+          'posts_per_page' => $num_posts,
+          'orderby' => 'post_date'
+        );
+
+        $query = new WP_Query( $args );
+
+      ?>
+      <?php $i=0; if( $query->have_posts() ) : while( $query->have_posts() ) : $query->the_post(); ?>
+        <?php if( $i < 4 ) : ?>
       <div class="col-lg-6">
         <div class="portCont">
-          <h4 class="text-center">Title of project</h4>
+          <h4 class="text-center"><?php the_title(); ?></h4>
           <div class="fullCenter imgCont shadow">
-            <img src="./assets/img/lukasz-szmigiel-33891-unsplash.jpg" alt="">
+            <?php the_post_thumbnail(); ?>
             <i class="far fa-hand-pointer tap dNonePc"></i>
             <div class="overlay">
-              <h4 class="centered">This is the test text</h4>
-              <a href="#" class="link"><i class="fas fa-link"></i></a>
-              <a href="#" class="read"><i class="fab fa-readme"></i></a>
+              <h4 class="centered"><?php the_field( 'hover_text' ); ?></h4>
+              <a href="<?php the_field( 'website_link' ); ?>" class="link"><i class="fas fa-link"></i></a>
+              <a href="<?php the_permalink(); ?>" class="read"><i class="fab fa-readme"></i></a>
             </div>
           </div>
         </div>
       </div>
-      <div class="col-lg-6">
+    <?php endif; ?>
+      <?php $i++;?>
+      <?php endwhile; endif; wp_reset_postdata(); ?>
+
+</div>
+      <!-- <div class="col-lg-6">
         <div class="portCont">
           <h4 class="text-center">Title of project</h4>
           <div class="fullCenter imgCont shadow">
@@ -64,7 +84,7 @@
           </div>
         </div>
       </div>
-    </div>
+    </div> -->
     <div class="row pad-top-cont">
       <div class="col-lg-12">
         <button type="button" class="btn btn-primary float-right">See My Whole Portfolio</button>
