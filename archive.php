2db12555c9ea27dcd68d7b20c5b723aca874e60e
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-01-25 20:10:23 +0000

    archive php

--- archive.php
+++ archive.php
@@ -118,4 +118,28 @@ get_header(); ?>
     </div>
   </div>
 </div>
+<?php $args = array(
+	'show_option_all'    => '',
+	'show_option_none'   => '',
+	'option_none_value'  => '-1',
+	'orderby'            => 'ID',
+	'order'              => 'ASC',
+	'show_count'         => 0,
+	'hide_empty'         => 1,
+	'child_of'           => 0,
+	'exclude'            => '',
+	'include'            => '',
+	'echo'               => 1,
+	'selected'           => 0,
+	'hierarchical'       => 0,
+	'name'               => 'cat',
+	'id'                 => '',
+	'class'              => 'postform',
+	'depth'              => 0,
+	'tab_index'          => 0,
+	'taxonomy'           => 'category',
+	'hide_if_empty'      => false,
+	'value_field'	     => 'term_id',
+); ?>
+<?php wp_dropdown_categories( $args ); ?> 
 <?php get_footer(); ?>
