ebb4270796c96f9a2f3b86c9d0b6c59c95e7562e
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-01-29 16:16:04 +0000

    taxonomy drop down test

--- archive.php
+++ archive.php
@@ -78,48 +78,7 @@ get_header(); ?>
 
 </section>
 
-<?php
 
-  $num_posts = get_option( 'posts_per_page' );
-  $post_types = array(
-    'post',
-    'designs',
-    'portfolio',
-    'book_review'
-  );
-  $args = array(
-    'post_type' => $post_types,
-    'posts_per_page' => $num_posts,
-    'orderby' => 'post_date'
-  );
-
-  $query = new WP_Query( $args );
-
-?>
-<?php $i=0; if( $query->have_posts() ) : while( $query->have_posts() ) : $query->the_post(); ?>
-<div class="container post curvedCorner bottomBorderRandom shadow margin-topBot">
-  <div class="row postCont">
-    <div class="col-lg-6">
-      <div class="post-textCont">
-        <h2 class="postHeading"><?php the_title(); ?></h2>
-        <p class="padAll">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-      </div>
-    </div>
-    <div class="col-lg-6">
-      <img class="margin-topBot shadow-sm" src="./assets/img/lukasz-szmigiel-33891-unsplash.jpg" alt="">
-    </div>
-  </div>
-  <div class="row postButtonCont">
-    <div class="col-lg-12">
-      <div class="postButton">
-        <a href="#" class="btn btn-primary btn-lg active float-right" role="button" aria-pressed="true">Primary link</a>
-      </div>
-    </div>
-  </div>
-</div>
-<?php endwhile; endif; wp_reset_postdata(); ?>
 <div class="container post curvedCorner bottomBorderRandom shadow margin-topBot">
   <div class="row postCont">
     <div class="col-lg-6">
