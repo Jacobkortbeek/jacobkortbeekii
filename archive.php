4e8eff2c88b4e37317cde412cbc0263f8e2cae87
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-01-29 15:33:28 +0000

    taxonomy drop down test

--- archive.php
+++ archive.php
@@ -46,8 +46,28 @@ get_header(); ?>
 
 			<!-- Display each post's title and content - you can change this section to display whatever post content you want. -->
 			<article class="tool-entry">
-				<h1><?php the_title(); ?></h1>
-				<p><?php the_content(); ?></p>
+        <div class="container post curvedCorner bottomBorderRandom shadow margin-topBot">
+          <div class="row postCont">
+            <div class="col-lg-6">
+              <div class="post-textCont">
+                <h2 class="postHeading"><?php the_title(); ?></h2>
+                <p class="padAll">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <img class="margin-topBot shadow-sm" src="./assets/img/lukasz-szmigiel-33891-unsplash.jpg" alt="">
+            </div>
+          </div>
+          <div class="row postButtonCont">
+            <div class="col-lg-12">
+              <div class="postButton">
+                <a href="#" class="btn btn-primary btn-lg active float-right" role="button" aria-pressed="true">Primary link</a>
+              </div>
+            </div>
+          </div>
+        </div>
 			</article>
 
 		<?php endwhile; ?>
@@ -57,25 +77,6 @@ get_header(); ?>
 	<?php endif; ?>
 
 </section>
-
-
-<!-- <div class="container postFilter">
-  <div class="row">
-    <div class="col-lg-12">
-      <div class="dropdown">
-        <button class="dropBtn shadow-sm">Filter</button>
-        <div class="dropdownContent shadow" id="dropDownCont">
-          <a href="#">Link</a>
-          <a href="#">Link</a>
-          <a href="#">Link</a>
-          <a href="#">Link</a>
-          <a href="#">Link</a>
-          <a href="#">Link</a>
-        </div>
-      </div>
-    </div>
-  </div>
-</div> -->
 
 <?php
 
