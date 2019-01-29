adfb4bb85cff6d7a8fd2e5dca0e4eef33deecae1
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-01-29 19:38:05 +0000

    taxonomy drop down test

--- archive.php
+++ archive.php
@@ -52,7 +52,7 @@ get_header(); ?>
               <div class="post-textCont">
                 <h2 class="postHeading"><?php the_title(); ?></h2>
                 <div class="padAll">
-                  <?php the_content(); ?>
+                  <?php the_excerpt(); ?>
                 </div>
               </div>
             </div>
@@ -80,52 +80,5 @@ get_header(); ?>
 	<?php endif; ?>
 
 </section>
-
-
-<!-- <div class="container post curvedCorner bottomBorderRandom shadow margin-topBot">
-  <div class="row postCont">
-    <div class="col-lg-6">
-      <div class="post-textCont">
-        <h2 class="postHeading">Title 11</h2>
-        <p class="padAll">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-      </div>
-    </div>
-    <div class="col-lg-6">
-      <img class="margin-topBot shadow-sm" src="./assets/img/lukasz-szmigiel-33891-unsplash.jpg" alt="">
-    </div>
-  </div>
-  <div class="row postButtonCont">
-    <div class="col-lg-12">
-      <div class="postButton">
-        <a href="#" class="btn btn-primary btn-lg active float-right" role="button" aria-pressed="true">Primary link</a>
-      </div>
-    </div>
-  </div>
-</div>
-
-<div class="container post curvedCorner bottomBorderRandom shadow margin-topBot">
-  <div class="row postCont">
-    <div class="col-lg-6">
-      <div class="post-textCont">
-        <h2 class="postHeading">Title</h2>
-        <p class="padAll">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-      </div>
-    </div>
-    <div class="col-lg-6">
-      <img class="margin-topBot shadow-sm" src="./assets/img/lukasz-szmigiel-33891-unsplash.jpg" alt="">
-    </div>
-  </div> -->
-  <div class="row postButtonCont">
-    <div class="col-lg-12">
-      <div class="postButton">
-        <a href="#" class="btn btn-primary btn-lg active float-right" role="button" aria-pressed="true">Primary link</a>
-      </div>
-    </div>
-  </div>
-</div>
 
 <?php get_footer(); ?>
