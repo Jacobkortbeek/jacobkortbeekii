6d8787771373b9d00b478cf73b51b2f8499662be
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-01-30 16:34:45 +0000

    single portfolio added

--- single.php
+++ single.php
@@ -1,1 +1,21 @@
-This is the single page
+<?php get_header(); ?>
+<div class="post">
+  <?php while ( have_posts() ) : the_post(); ?>
+    <div class="contianer-fluid fullCenter heading">
+      <div class="row">
+        <div class="col-lg-12">
+          <h2><?php the_title(); ?></h2>
+        </div>
+      </div>
+    </div>
+
+    <div class="container pad-top-cont">
+      <div class="row">
+        <div class="col-lg-12">
+          <?php the_content(); ?>
+        </div>
+      </div>
+    </div>
+  <?php endwhile; ?>
+</div>
+<?php get_footer(); ?>
