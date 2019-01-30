0b40f1edf6c68c38a3d1195fa712093103fdbfcd
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-01-30 16:18:53 +0000

    single portfolio added

--- single-designs.php
+++ single-designs.php
@@ -1,3 +1,30 @@
 <?php get_header(); ?>
-This is the single design page  
+
+<?php while ( have_posts() ) : the_post(); ?>
+<div class="contianer-fluid fullCenter heading">
+  <div class="row">
+    <div class="col-lg-12">
+      <h2><?php the_title(); ?></h2>
+    </div>
+  </div>
+</div>
+
+<div class="container img pad-top-cont">
+  <div class="row">
+    <div class="col-lg-12">
+      <?php the_post_thumbnail(); ?>
+      <a href="#" class="round fullCenter"><i class="fas fa-link"></i></a>
+    </div>
+  </div>
+</div>
+
+<div class="container pad-top-cont">
+  <div class="row">
+    <div class="col-lg-12">
+      <?php the_content(); ?>
+    </div>
+  </div>
+</div>
+</main>
+<?php endwhile; ?>
 <?php get_footer(); ?>
