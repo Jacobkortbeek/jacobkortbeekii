ff947a1bbd6acc9a9f26e6828ace48eab97f2c58
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-02-01 22:48:31 +0000

    comitting new styp php

--- functions.php
+++ functions.php
@@ -11,7 +11,6 @@ function wplift_pagination() {
 			'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
 			'format' => '?paged=%#%',
 			'current' => max( 1, get_query_var('paged') ),
-			'total' => $wp_query->max_num_pages,
       'paged' => $GLOBALS['paged']
 		) );
 }
