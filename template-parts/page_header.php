5e417379578dd89c25f9bbe3793530bd86110c12
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-02-01 21:31:02 +0000

    comitting new styp php

--- template-parts/page_header.php
+++ template-parts/page_header.php
@@ -15,7 +15,7 @@
 <?php endif; ?>
   <div class="row">
     <div class="col-lg-12">
-      <h1><?php the_sub_field('page_header_title'); ?></h1>
+      <h1 class="centered"><?php the_sub_field('page_header_title'); ?></h1>
     </div>
   </div>
 </div>
