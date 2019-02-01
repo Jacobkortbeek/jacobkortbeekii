5569fa7fe085b719981fb8b145c43f57e49f4cca
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-02-01 22:29:49 +0000

    comitting new styp php

--- functions.php
+++ functions.php
@@ -105,6 +105,12 @@ return new WP_Query( array(
     'orderby' => 'post_date',
     'tax_query'      => km_get_tools_in_taxonomy_term_tax_query(),
     'paged' => $GLOBALS['paged'],
+    echo paginate_links( array(
+    'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+    'format' => '?paged=%#%',
+    'current' => max( 1, get_query_var('paged') ),
+    'total' => $wp_query->max_num_pages
+  ) );
 ) );
 }
 /**
