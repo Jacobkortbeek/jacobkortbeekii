f1fa99d3003d2925ff68fb8f7df61e344405fc72
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-02-01 22:58:04 +0000

    comitting new styp php

--- functions.php
+++ functions.php
@@ -2,91 +2,6 @@
 
 ini_set( 'mysql.trace_mode', 0 );
 
-// Numbered Pagination
-function wplift_pagination() {
-	// global $wp_query;
-    // $published_posts = wp_count_posts()->publish;
-    // $posts_per_page = get_option('posts_per_page');
-    // $page_number_max = ceil($published_posts / $posts_per_page);
-		$big = 999999999; // need an unlikely integer
-			echo paginate_links( array(
-			'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-			'format' => '?paged=%#%',
-			'current' => max( 1, get_query_var('paged') ),
-			'total' => 99
-		) );
-}
-
-
-function wpbeginner_numeric_posts_nav() {
-
-    if( is_singular() )
-        return;
-
-    global $wp_query;
-    /** Stop execution if there's only 1 page */
-    if( $wp_query->max_num_pages <= 1 )
-        return;
-
-    $paged = get_query_var( 'paged' ) ? absint( get_query_var( 'paged' ) ) : 1;
-    $max   = intval( $wp_query->max_num_pages );
-
-    /** Add current page to the array */
-    if ( $paged >= 1 )
-        $links[] = $paged;
-
-    /** Add the pages around the current page to the array */
-    if ( $paged >= 3 ) {
-        $links[] = $paged - 1;
-        $links[] = $paged - 2;
-    }
-
-    if ( ( $paged + 2 ) <= $max ) {
-        $links[] = $paged + 2;
-        $links[] = $paged + 1;
-    }
-
-    echo '<div class="navigation"><ul>' . "\n";
-
-    /** Previous Post Link */
-    if ( get_previous_posts_link() )
-        printf( '<li>%s</li>' . "\n", get_previous_posts_link() );
-
-    /** Link to first page, plus ellipses if necessary */
-    if ( ! in_array( 1, $links ) ) {
-        $class = 1 == $paged ? ' class="active"' : '';
-
-        printf( '<li%s><a href="%s">%s</a></li>' . "\n", $class, esc_url( get_pagenum_link( 1 ) ), '1' );
-
-        if ( ! in_array( 2, $links ) )
-            echo '<li>…</li>';
-    }
-
-    /** Link to current page, plus 2 pages in either direction if necessary */
-    sort( $links );
-    foreach ( (array) $links as $link ) {
-        $class = $paged == $link ? ' class="active"' : '';
-        printf( '<li%s><a href="%s">%s</a></li>' . "\n", $class, esc_url( get_pagenum_link( $link ) ), $link );
-    }
-
-    /** Link to last page, plus ellipses if necessary */
-    if ( ! in_array( $max, $links ) ) {
-        if ( ! in_array( $max - 1, $links ) )
-            echo '<li>…</li>' . "\n";
-
-        $class = $paged == $max ? ' class="active"' : '';
-        printf( '<li%s><a href="%s">%s</a></li>' . "\n", $class, esc_url( get_pagenum_link( $max ) ), $max );
-    }
-
-    /** Next Post Link */
-    if ( get_next_posts_link() )
-        printf( '<li>%s</li>' . "\n", get_next_posts_link() );
-
-    echo '</ul></div>' . "\n";
-
-}
-
-
 
 /**
 * Get tools, filtered by the taxonomy term, if one was selected.
@@ -138,7 +53,22 @@ return array();
 function km_get_selected_taxonomy_dropdown_term() {
 return isset( $_GET[ 'category' ] ) && $_GET[ 'category' ] ? sanitize_text_field( $_GET[ 'category' ] ) : '';
 }
+$tools_in_taxonomy_term = km_get_tools_in_taxonomy_term();
 
+// Numbered Pagination
+function wplift_pagination() {
+	// global $wp_query;
+    // $published_posts = wp_count_posts()->publish;
+    // $posts_per_page = get_option('posts_per_page');
+    // $page_number_max = ceil($published_posts / $posts_per_page);
+		$big = 999999999; // need an unlikely integer
+			echo paginate_links( array(
+			'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+			'format' => '?paged=%#%',
+			'current' => max( 1, get_query_var('paged') ),
+			'total' => $tools_in_taxonomy_term->max_num_pages
+		) );
+}
 
 
 
