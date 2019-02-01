8a677bc7b66136c7ad421e171febe449d294b3df
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-02-01 20:01:20 +0000

    comitting new styp php

--- template-parts/page_header.php
+++ template-parts/page_header.php
@@ -2,7 +2,18 @@
 
 
 
-<div class="container-fluid headerAbout fullCenter" style="background-image: linear-gradient( <?php the_sub_field( 'page_hader_rgb_color_value' ); ?>, <?php the_sub_field( 'page_hader_rgb_color_value' ); ?>), url('<?php the_sub_field('header_page_image'); ?>'); color: <?php the_sub_field( 'heading_text_color' ); ?>;">
+<div class="container-fluid headerAbout fullCenter" style="color: <?php the_sub_field( 'heading_text_color' ); ?>;
+<?php
+  $pageHeader = get_sub_filed( 'page_header_image' );
+  if($pageHeader['image']) :
+ ?>
+    background-image: linear-gradient( <?php the_sub_field( 'page_hader_rgb_color_value' ); ?>, <?php the_sub_field( 'page_hader_rgb_color_value' ); ?>), url('<?php the_sub_field('header_page_image'); ?>');
+  <?php
+    elseif($pageHeader['color']) :
+  ?>
+  background-color: <?php the_sub_field( 'page_header_background_color' ); ?>;
+<?php endif; ?>
+">
   <div class="row">
     <div class="col-lg-12">
       <h1><?php the_sub_field('page_header_title'); ?></h1>
