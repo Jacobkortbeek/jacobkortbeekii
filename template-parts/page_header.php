781f85277a5ff10e81d0932bf8483ee0ee979d89
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-02-01 20:03:10 +0000

    comitting new styp php

--- template-parts/page_header.php
+++ template-parts/page_header.php
@@ -7,13 +7,12 @@
   $pageHeader = get_sub_filed( 'page_header_image' );
   if($pageHeader['image']) :
  ?>
-    background-image: linear-gradient( <?php the_sub_field( 'page_hader_rgb_color_value' ); ?>, <?php the_sub_field( 'page_hader_rgb_color_value' ); ?>), url('<?php the_sub_field('header_page_image'); ?>');
+    background-image: linear-gradient( <?php the_sub_field( 'page_hader_rgb_color_value' ); ?>, <?php the_sub_field( 'page_hader_rgb_color_value' ); ?>), url('<?php the_sub_field('header_page_image'); ?>'); ">
   <?php
     elseif($pageHeader['color']) :
   ?>
-  background-color: <?php the_sub_field( 'page_header_background_color' ); ?>;
+  background-color: <?php the_sub_field( 'page_header_background_color' ); ?>;">
 <?php endif; ?>
-">
   <div class="row">
     <div class="col-lg-12">
       <h1><?php the_sub_field('page_header_title'); ?></h1>
