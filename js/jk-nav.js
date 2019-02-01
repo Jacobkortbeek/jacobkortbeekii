bc9d70975101e6119b6e2f1c135b199fb9a1f953
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-02-01 13:44:48 +0000

    webfonts

--- js/jk-nav.js
+++ js/jk-nav.js
@@ -1,10 +1,10 @@
 jQuery(document).ready(function($) {
   $(window).on('load', () => {
-    $('.jk-nav i').on('click', () => {
+    $('.jk-nav').on('click', () => {
       console.log('nav click');
       $('.jk-nav-cont').addClass('showNav');
     });
-    $('.jk-nav-cont i').on('click', () => {
+    $('.jk-nav-cont').on('click', () => {
       console.log('nav X click');
       $('.jk-nav-cont').removeClass('showNav');
     });
